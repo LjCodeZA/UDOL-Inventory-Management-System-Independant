2bcf3c7400686d8ef7641be7a62ad054cc57c7eb
LjCodeZA <75343843+LjCodeZA@users.noreply.github.com>
2020-12-02 21:48:11 +0200

    Added project to Github
    Added project to Github

---
+++ InventoryManagementSystem/Controllers/StockOutController.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagementSystem.DAL;
+using InventoryManagementSystem.Models;
+using InventoryManagementSystem.POCO;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class StockOutController : Controller
+    {
+        private IMSContext db = new IMSContext();
+
+        // GET: StockOuts
+        public ActionResult Index()
+        {
+            return View(db.StockOut.ToList());
+        }
+
+        // GET: StockOuts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockOut stockOut = db.StockOut.Find(id);
+            if (stockOut == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockOut);
+        }
+
+        // GET: StockOuts/Create
+        public ActionResult Create()
+        {
+            var vendorProduct = (from productVendor in db.ProductVendor
+                                 join vendor in db.Vendor
+                                   on productVendor.VendorId equals vendor.VendorId
+                                 join product in db.Product
+                                  on productVendor.ProductId equals product.ProductId
+                                 select new SelectListItem
+                                 {
+                                     Value = productVendor.ProductVendorId.ToString(),
+                                     Text = productVendor.ProductVendorId.ToString() + " - " + vendor.Name + " - " + product.Name
+                                 }).ToList();
+
+            ViewBag.ProductVendorId = vendorProduct;
+            return View();
+        }
+
+        // POST: StockOuts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StockOutId,ProductVendorId,Quantity,Recon,CreatedDate")] StockOut stockOut)
+        {
+            if (ModelState.IsValid)
+            {
+                DoStockAllocation(stockOut);
+                return RedirectToAction("Index");
+            }
+
+            return View(stockOut);
+        }
+
+        // GET: StockOuts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockOut stockOut = db.StockOut.Find(id);
+            if (stockOut == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockOut);
+        }
+
+        // POST: StockOuts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StockOutId,ProductVendorId,Quantity,CreatedDate")] StockOut stockOut)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stockOut).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(stockOut);
+        }
+
+        // GET: StockOuts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockOut stockOut = db.StockOut.Find(id);
+            if (stockOut == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockOut);
+        }
+
+        // POST: StockOuts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StockOut stockOut = db.StockOut.Find(id);
+            db.StockOut.Remove(stockOut);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public void DoStockAllocation(StockOut stockOut)
+        {
+            var dbContext = new IMSContext();
+
+            var productId = (from productVendor in dbContext.ProductVendor where productVendor.ProductVendorId == stockOut.ProductVendorId select productVendor.ProductId).FirstOrDefault();
+          
+            var allVendorsForProduct = (from productVendors in dbContext.ProductVendor where productVendors.ProductId == productId select productVendors); //Exclude current selected vendor
+
+            //Does the selected vendor have enough stock?
+            var stockTakenOutButNotReconned = (from stockTakenOut in dbContext.StockOut where stockTakenOut.ProductVendorId == stockOut.ProductVendorId && (stockTakenOut.Recon == false || stockTakenOut.Recon == null) select (int?)stockTakenOut.Quantity).Sum() ?? 0;
+
+            var latestStockTake = (from productStock in dbContext.ProductStock
+                                   group productStock by productStock.ProductVendorId
+                                   into latestStock
+                                   select latestStock.OrderByDescending(t => t.StockTakeDate).FirstOrDefault());
+
+            var amountOfStockReconnedForVendor = latestStockTake.Where(i => i.ProductVendorId == stockOut.ProductVendorId).Select(o => (int?)o.Quantity).Sum() ?? 0;
+
+            if (amountOfStockReconnedForVendor - (stockTakenOutButNotReconned + stockOut.Quantity) < 0)
+            {
+                //Not enough stock for order from this vendor. Need to check different vendors for stock.
+                //We start off by checking if we have enough in total.
+                var stockTakenOutButNotReconnedForProductVendors = (from stockTakenOut in dbContext.StockOut where allVendorsForProduct.Select(i => i.ProductVendorId).Contains(stockTakenOut.ProductVendorId) && (stockTakenOut.Recon == false || stockTakenOut.Recon == null) select (int?)stockTakenOut.Quantity).Sum() ?? 0;
+                var amountOfStockReconnedForProductVendors = latestStockTake.Where(i => allVendorsForProduct.Select(vendors => vendors.ProductVendorId).Contains(i.ProductVendorId)).Select(o => o.Quantity).Sum();
+
+                if (amountOfStockReconnedForProductVendors - (stockTakenOutButNotReconnedForProductVendors + stockOut.Quantity) >= 0)
+                {
+                    //We have enough total stock to service the request
+
+                    var totalStockRequested = stockOut.Quantity;
+                    var remainingStockToAllocate = totalStockRequested;
+
+                    //Start by allocating stock from chosen vendor
+
+                    var currentVendorStock = amountOfStockReconnedForVendor - stockTakenOutButNotReconned;
+
+
+                    stockOut.Quantity = currentVendorStock;
+                    db.StockOut.Add(stockOut);
+                    db.SaveChanges();
+
+                    remainingStockToAllocate -= currentVendorStock;
+
+                    var alternativeProductVendors = (from productVendors in dbContext.ProductVendor where productVendors.ProductVendorId != stockOut.ProductVendorId && productVendors.ProductId == productId select productVendors); //Exclude current selected vendor
+                    foreach (var item in alternativeProductVendors)
+                    {
+                        var amountOfStockReconnedForAlternativeVendor = latestStockTake.Where(i => i.ProductVendorId == item.ProductVendorId).Select(o => o.Quantity).Sum();
+                        var stockTakenOutButNotReconnedForAlternativeVendor = (from stockTakenOut in dbContext.StockOut where stockTakenOut.ProductVendorId == item.ProductVendorId && (stockTakenOut.Recon == false || stockTakenOut.Recon == null) select (int?)stockTakenOut.Quantity).Sum() ?? 0;
+
+                        currentVendorStock = amountOfStockReconnedForAlternativeVendor - stockTakenOutButNotReconnedForAlternativeVendor;
+                        if (currentVendorStock == 0)
+                            continue;
+
+                        if (currentVendorStock >= remainingStockToAllocate)
+                        {
+                            db.StockOut.Add(new StockOut()
+                            {
+                                ProductVendorId = item.ProductVendorId,
+                                CreatedDate = DateTime.Now,
+                                Quantity = remainingStockToAllocate,
+                                Recon = null
+                            });
+
+                            db.SaveChanges();
+
+                            break;
+                        }
+                        else
+                        {
+                            db.StockOut.Add(new StockOut()
+                            {
+                                ProductVendorId = item.ProductVendorId,
+                                CreatedDate = DateTime.Now,
+                                Quantity = currentVendorStock,
+                                Recon = null
+                            });
+
+                            db.SaveChanges();
+                        }
+
+                        remainingStockToAllocate -= currentVendorStock;
+                    }
+                }
+                else
+                {
+                    //We don't have enough stock at all. Stop process.
+                    return;
+                }
+            }
+            else
+            {
+                //Enough stock from the selected vendor. Carry on with order.
+
+                db.StockOut.Add(stockOut);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ InventoryManagementSystem/DAL/IMSInitializer.cs
@@ -0,0 +1,66 @@
+﻿using InventoryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+
+namespace InventoryManagementSystem.DAL
+{
+    public class IMSInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<IMSContext>
+    {
+        protected override void Seed(IMSContext context)
+        {
+            var products = new List<Product>
+            {
+                new Product{ ProductId=1,Name="Logitech MX518",Description="Gaming Mouse" },
+                new Product{ ProductId=2,Name="Razor DeathAdder Chroma",Description="Gaming Mouse" },
+                new Product{ ProductId=3,Name="Steelseries Rival 310",Description="Gaming Mouse" },
+                new Product{ ProductId=4,Name="Corsair Void",Description="Gaming Headset" },
+                 new Product{ ProductId=5,Name="Steelseries arctis 3",Description="Designed for everywhere you game, with superior sound, comfort and style on all gaming platforms, including PC, PlayStation..." },
+                new Product{ ProductId=6,Name="Kingston Hyper X Cloud 2",Description="Advanced USB audio control box with built-in DSP sound card, Hi-Fi capable with 53mm drivers for supreme audio quality..." }
+            };
+            products.ForEach(s => context.Product.Add(s));
+            context.SaveChanges();
+
+            var vendor = new List<Vendor>
+            {
+                new Vendor { VendorId=1,Name="MouseCo",Description="Mouse provider" },
+                new Vendor { VendorId=2,Name="Logitech",Description="Mouse provider" }
+            };
+            vendor.ForEach(s => context.Vendor.Add(s));
+            context.SaveChanges();
+
+            var productVender = new List<ProductVendor>
+            {
+                new ProductVendor{ ProductVendorId=1,ProductId=1,VendorId=1,Price=500f, ListPrice=600f },
+                new ProductVendor{ ProductVendorId=2,ProductId=1,VendorId=2,Price=550f, ListPrice=600f },
+                new ProductVendor{ ProductVendorId=3,ProductId=4,VendorId=1,Price=1200f, ListPrice=1400f },
+                new ProductVendor{ ProductVendorId=4,ProductId=4,VendorId=2,Price=1300f, ListPrice=1400f },
+                new ProductVendor{ ProductVendorId=5,ProductId=2,VendorId=1,Price=700f, ListPrice=1000f },
+                new ProductVendor{ ProductVendorId=6,ProductId=3,VendorId=1,Price=300f, ListPrice=600f },
+                new ProductVendor{ ProductVendorId=7,ProductId=5,VendorId=1,Price=1500f, ListPrice=1800f },
+                new ProductVendor{ ProductVendorId=8,ProductId=6,VendorId=1,Price=800f, ListPrice=1000f },
+            };
+            productVender.ForEach(s => context.ProductVendor.Add(s));
+            context.SaveChanges();
+
+            var productStock = new List<ProductStock>
+            {
+                new ProductStock{ ProductStockId=1, ProductVendorId=1, Quantity=50, StockTakeDate=DateTime.Now},
+                new ProductStock{ ProductStockId=2, ProductVendorId=2, Quantity=50, StockTakeDate=DateTime.Now},
+                new ProductStock{ ProductStockId=3, ProductVendorId=3, Quantity=20, StockTakeDate=DateTime.Now},
+                new ProductStock{ ProductStockId=4, ProductVendorId=4, Quantity=40, StockTakeDate=DateTime.Now},
+                new ProductStock{ ProductStockId=5, ProductVendorId=4, Quantity=60, StockTakeDate=DateTime.Now.AddDays(-1)},
+            };
+            productStock.ForEach(s => context.ProductStock.Add(s));
+            context.SaveChanges();
+
+            var stockOut = new List<StockOut>
+            {
+                new StockOut { StockOutId = 1, ProductVendorId = 1, Quantity = 20, CreatedDate=DateTime.Now },
+                new StockOut { StockOutId = 2, ProductVendorId = 3, Quantity = 10, CreatedDate=DateTime.Now }
+            };
+            stockOut.ForEach(s => context.StockOut.Add(s));
+            context.SaveChanges();
+
+        }
+    }
+}
